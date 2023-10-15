d79403203994155f73f28dcd366af5d383ac656f
Jakub Urbański <jakuburbanski2007@gmail.com>
2023-10-15 22:53:37 +0200

    Added home import

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Home } from './components/Home/Home'
 import { Navbar } from './components/Navbar/Navbar'
 
 export const App = () => {
 	return (
 		<>
 			<Navbar />
+			<Home />
 		</>
 	)
 }

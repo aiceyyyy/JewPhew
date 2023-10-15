13a9eac53234585c74e91ca589cc5c486b75347a
Jakub Urbański <jakuburbanski2007@gmail.com>
2023-10-15 22:55:11 +0200

    Added home section (not working button yet)

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,26 @@
+import path from '../../assets/Screenshot_3.png'
+import classes from './Home.module.css'
+import { HiChevronDoubleDown } from 'react-icons/hi2'
+
+export const Home = () => {
+	return (
+		<div className='flex flex-col justify-start items-center mt-[100px] lg:mt-[200px] w-[100vw] h-[calc(100vh-100px)] z-10 mx-auto'>
+			<div className='flex flex-col lg:flex-row mx-auto px-auto'>
+				<img className='xs:max-w-[150px] w-[150px] h-[150px] mx-auto xs:mt-5 lg:min-w-[450px] lg:w-[450px] lg:h-[450px] mb-auto' src={path} alt='diamond' />
+				<div className='h-3/5 mt-5 mb-20 xs:p-10 lg:px-24'>
+					<h2 className='max-w-[300px] mb-7 text-4xl uppercase font-thin tracking-wide text-gray-600 xs:text-center lg:text-left'>Bestseller</h2>
+					<p className='max-w-[300px] mb-5 text-3xl font-bold xs:text-center lg:text-left'>Diamond</p>
+					<p className='max-w-[300px] xs:text-center lg:text-left'>
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero labore quas mollitia et enim, similique sed!
+						Repudiandae, amet! Nostrum amet fugit illum id modi reprehenderit, incidunt ipsa mollitia nam aut! Delectus
+						itaque aut sed fuga aperiam alias veritatis, veniam ipsa?
+					</p>
+				</div>
+			</div>
+            //! Fix button position
+			<button className={`absolute mx-auto left-1/2 bottom-10 rounded-full xs:p-4 lg:p-6 text-2xl ${classes.bounce}`}>
+				<HiChevronDoubleDown />
+			</button>
+		</div>
+	)
+}

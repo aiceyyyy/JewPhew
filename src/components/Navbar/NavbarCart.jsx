18e2840b0738a33f353bb59824655909cfce51ab
Jakub Urbański <jakuburbanski2007@gmail.com>
2023-10-16 19:06:46 +0200

    Fixed navbar UI

--- src/components/Navbar/NavbarCart.jsx
+++ src/components/Navbar/NavbarCart.jsx
@@ -20,7 +20,7 @@ export const NavbarCart = () => {
 
 	return (
 		<div className='flex xs:p-3 sm:p-5 justify-between items-center text-4xl'>
-			<IoBagOutline className='relative xs:mr-5 lg:mr-10 text-white hover:text-purple-500 transition-all duration-300 cursor-pointer' />
+			<IoBagOutline className='relative xs:mr-5 lg:mr-0  text-white hover:text-purple-500 transition-all duration-300 cursor-pointer' />
 			<span
 				className='absolute top-1/2 flex justify-center items-center ml-5 bg-slate-300 w-[20px] h-[20px] text-sm rounded-full text-slate-700 font-bold' >
 				1

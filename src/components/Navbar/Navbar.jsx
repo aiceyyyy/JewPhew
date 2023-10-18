1f32a6b66914492cd4e28903f69454bf0026e111
Jakub Urbański <jakuburbanski2007@gmail.com>
2023-10-18 22:40:05 +0200

    Changed text font

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
 import { NavbarCart } from './NavbarCart'
+import Font from 'react-font'
 
 export const Navbar = () => {
-
 	const navTexts = [
 		{
 			id: 1,
@@ -50,27 +50,29 @@ export const Navbar = () => {
 	}, [])
 
 	return (
-		<div
-			style={{ backgroundColor: dynamicNavStyle }}
-			className='w-screen flex justify-between fixed top-0 left-0 p-1 px-auto xs:px-1 sm:px-10 transition-all duration-300 z-40'>
-			<div className='flex justify-center items-center p-3'>
-				<a
-					className='text-3xl font-bold text-white hover:text-purple-500 transition-all duration-300 cursor-pointer'
-					href='#'>
-					JewPhew
-				</a>
+		<Font family='Open Sans'>
+			<div
+				style={{ backgroundColor: dynamicNavStyle }}
+				className='w-screen flex justify-between fixed top-0 left-0 p-1 px-auto xs:px-1 sm:px-10 transition-all duration-300 z-40'>
+				<div className='flex justify-center items-center p-3'>
+					<a
+						className='text-3xl font-bold text-white hover:text-purple-500 transition-all duration-300 cursor-pointer'
+						href='#'>
+						JewPhew
+					</a>
+				</div>
+				<div className='flex justify-center items-center mr-auto ml-10 lg:ml-20 px-auto'>
+					{screenWidth > 798 &&
+						navTexts.map(item => (
+							<a
+								className='text-lg text-white hover:text-purple-500 transition-all duration-300 font-semibold ml-10 cursor-pointer'
+								key={item.id}>
+								{item.content}
+							</a>
+						))}
+				</div>
+				<NavbarCart />
 			</div>
-			<div className='flex justify-center items-center mr-auto ml-10 lg:ml-20 px-auto'>
-				{screenWidth > 798 &&
-					navTexts.map(item => (
-						<a
-							className='text-lg text-white hover:text-purple-500 transition-all duration-300 font-semibold ml-10 cursor-pointer'
-							key={item.id}>
-							{item.content}
-						</a>
-					))}
-			</div>
-			<NavbarCart/>
-		</div>
+		</Font>
 	)
 }

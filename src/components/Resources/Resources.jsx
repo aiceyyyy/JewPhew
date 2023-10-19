4f24de55a005667cf77fb29ba5b2b0a4c696770d
Jakub Urbański <jakuburbanski2007@gmail.com>
2023-10-19 21:36:07 +0200

    Corrected styles, colors

--- src/components/Resources/Resources.jsx
+++ src/components/Resources/Resources.jsx
@@ -6,7 +6,7 @@ export const Resources = () => {
 	return (
 		<div className='w-screen h-[90vh] flex justify-center flex-wrap mx-auto'>
 			<div className='max-w-[250px] mx-3 my-3 py-5 px-3 rounded-xl'>
-				<h2 className='text-center text-2xl mb-5 italic'>Diamonds</h2>
+				<h2 className='text-center text-2xl mb-5 text-purple-500 tracking-wider uppercase'>Diamonds</h2>
 				<img className='h-[200px] rounded-xl' src={path} alt='diamonds' />
 				<p className='mt-3'>
 					Diamond is a solid form of the element carbon with its atoms arranged in a crystal structure called diamond
@@ -16,7 +16,7 @@ export const Resources = () => {
 				</p>
 			</div>
 			<div className='max-w-[250px] mx-20 my-3 py-5 px-3 rounded-xl'>
-				<h2 className='text-center text-2xl mb-5 italic'>Gold</h2>
+				<h2 className='text-center text-2xl mb-5 text-purple-500 tracking-wider uppercase'>Gold</h2>
 				<img className='h-[200px] rounded-xl' src={pic} alt='silver' />
 				<p className='mt-3'>
 					Gold is a chemical element with the symbol Au (from Latin aurum 'gold') and atomic number 79. It is a bright,
@@ -25,7 +25,7 @@ export const Resources = () => {
 				</p>
 			</div>
 			<div className='max-w-[250px] mx-3 my-3 py-5 px-3 rounded-xl'>
-				<h2 className='text-center text-2xl mb-5 italic'>Silver</h2>
+				<h2 className='text-center text-2xl mb-5 text-purple-500 tracking-wider uppercase'>Silver</h2>
 				<img className='h-[200px] rounded-xl' src={img} alt='gold' />
 				<p className='mt-3'>
 					Silver is a chemical element with the symbol Ag (from Latin argentum 'silver', derived from the

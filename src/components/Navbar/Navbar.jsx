d025afc2b991ffa40deec1561cc44e83ee7041a5
Jakub Urbański <jakuburbanski2007@gmail.com>
2023-10-20 22:50:19 +0200

    Added link anchors

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,18 +6,22 @@ export const Navbar = () => {
 		{
 			id: 1,
 			content: 'Home',
+			tp: '#'
 		},
 		{
 			id: 2,
 			content: 'Resources',
+			tp: '#diamonds',
 		},
 		{
 			id: 3,
 			content: 'Store',
+			tp: '#store'
 		},
 		{
 			id: 4,
 			content: 'Contact',
+			tp: '#contact'
 		},
 	]
 
@@ -49,27 +53,28 @@ export const Navbar = () => {
 	}, [])
 
 	return (
-			<div
-				style={{ backgroundColor: dynamicNavStyle }}
-				className='w-screen flex justify-between fixed top-0 left-0 p-1 px-auto xs:px-1 sm:px-10 transition-all duration-300 z-40'>
-				<div className='flex justify-center items-center p-3'>
-					<a
-						className='text-3xl font-bold text-white hover:text-purple-500 transition-all duration-300 cursor-pointer'
-						href='#'>
-						JewPhew
-					</a>
-				</div>
-				<div className='flex justify-center items-center mr-auto ml-10 lg:ml-20 px-auto'>
-					{screenWidth > 798 &&
-						navTexts.map(item => (
-							<a
-								className='text-lg text-white hover:text-purple-500 transition-all duration-300 font-semibold ml-10 cursor-pointer'
-								key={item.id}>
-								{item.content}
-							</a>
-						))}
-				</div>
-				<NavbarCart />
+		<div
+			style={{ backgroundColor: dynamicNavStyle }}
+			className='w-screen flex justify-between fixed top-0 left-0 p-1 px-auto xs:px-1 sm:px-10 transition-all duration-300 z-40'>
+			<div className='flex justify-center items-center p-3'>
+				<a
+					className='text-3xl font-bold text-white hover:text-purple-500 transition-all duration-300 cursor-pointer'
+					href={'#'}>
+					JewPhew
+				</a>
 			</div>
+			<div className='flex justify-center items-center mr-auto ml-10 lg:ml-20 px-auto'>
+				{screenWidth > 798 &&
+					navTexts.map(item => (
+						<a
+							href={item.tp}
+							className='text-lg text-white hover:text-white transition-all duration-300 font-semibold ml-10 cursor-pointer'
+							key={item.id}>
+							{item.content}
+						</a>
+					))}
+			</div>
+			<NavbarCart />
+		</div>
 	)
 }

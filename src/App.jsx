8d49945c50baf388e77c5caef37d2aab3006f187
Jakub Urbański <jakuburbanski2007@gmail.com>
2023-10-22 19:11:11 +0200

    Created Nav Status in App.jsx, and made it dynamic controlled by Mobile&NavCart components

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,26 @@ import { Resources } from './components/Resources/Resources'
 import { ProductSection } from './components/Products/ProductSection'
 import { Contact } from './components/Contact/Contact'
 import { Footer } from './components/Footer/Footer'
+import { useState } from 'react'
 
 export const App = () => {
+	const [navStatus, setNavStatus] = useState(false)
+
+	const closeNav = () => {
+		setNavStatus(false)
+	}
+
+	const openNav = () => {
+		setNavStatus(true)
+	}
+
 	return (
 		<>
-			<Mobile />
-			<Navbar />
+			<Mobile 
+				closeNav={closeNav} 
+				statusDispatch={navStatus}
+			/>
+			<Navbar openNav={openNav} />
 			<Home />
 			<Resources />
 			<ProductSection />

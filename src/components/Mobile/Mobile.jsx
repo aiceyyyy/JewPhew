e8bbd35c930ef84fe2417d7b20c6be9b8cb224c1
Jakub Urbański <jakuburbanski2007@gmail.com>
2023-10-22 19:12:49 +0200

    Added condition to nav reveal (closing nav with arrow/backdrop)

--- src/components/Mobile/Mobile.jsx
+++ src/components/Mobile/Mobile.jsx
@@ -1,4 +1,7 @@
-export const Mobile = () => {
+import { FaArrowRight } from 'react-icons/fa6'
+import classes from './Mobile.module.css'
+
+export const Mobile = props => {
 	const navTexts = [
 		{
 			id: 1,
@@ -22,5 +25,31 @@ export const Mobile = () => {
 		},
 	]
 
-	return <></>
+	return (
+		<div
+			id='nav'
+			className={`${classes.nav} ${
+				props.statusDispatch ? 'translate-x-[0%]' : 'translate-x-[-100%]'
+			} transition-all duration-300`}>
+			<div className='flex flex-col justify-center items-center w-2/5 bg-white border-r border-r-black text-black'>
+				<a href='#' className='text-xl text-black hover:text-purple-500 mb-10 transition-all duration-300'>
+					JewPhew
+				</a>
+				<button
+					onClick={props.closeNav}
+					className={`${classes.button} mb-10 py-3 px-5 bg-transparent hover:border-black transition-all duration-300`}>
+					<FaArrowRight />
+				</button>
+				{navTexts.map(text => (
+					<a
+						className='text-black p-1 my-5 hover:text-purple-500 transition-all duration-300'
+						href={text.tp}
+						key={text.id}>
+						{text.content}
+					</a>
+				))}
+			</div>
+			<div onClick={props.closeNav} className='w-3/5 backdrop-blur'></div>
+		</div>
+	)
 }

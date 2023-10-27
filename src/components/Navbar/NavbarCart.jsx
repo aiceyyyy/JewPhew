986dbfb6e34dd23734901b6a53331a784c6ace08
Jakub Urbański <jakuburbanski2007@gmail.com>
2023-10-27 19:23:45 +0200

    Added dynamic products in cart amount

--- src/components/Navbar/NavbarCart.jsx
+++ src/components/Navbar/NavbarCart.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { IoBagOutline } from 'react-icons/io5'
 import { FaBars } from 'react-icons/fa6'
 
-export const NavbarCart = (props) => {
+export const NavbarCart = props => {
 	const [screenWidth, setScreenWidth] = useState(window.innerWidth)
 
 	const handleResize = () => {
@@ -27,9 +27,14 @@ export const NavbarCart = (props) => {
 
 	return (
 		<div className='flex xs:p-3 sm:p-5 justify-between items-center text-4xl'>
-			<IoBagOutline onClick={openCart} className='relative xs:mr-5 lg:mr-0  text-white hover:text-white transition-all duration-300 cursor-pointer' />
-			<span onClick={openCart} className='absolute top-1/2 flex justify-center items-center ml-5 bg-slate-300 w-[20px] h-[20px] text-sm rounded-full text-slate-700 font-bold hover:text-purple-500 cursor-pointer transition-all duration-300'>
-				1
+			<IoBagOutline
+				onClick={openCart}
+				className='relative xs:mr-5 lg:mr-0  text-white hover:text-white transition-all duration-300 cursor-pointer'
+			/>
+			<span
+				onClick={openCart}
+				className='absolute top-1/2 flex justify-center items-center ml-5 bg-slate-300 w-[20px] h-[20px] text-sm rounded-full text-slate-700 font-bold hover:text-purple-500 cursor-pointer transition-all duration-300'>
+				{props.productAmount}
 			</span>
 			{screenWidth < 798 && (
 				<FaBars

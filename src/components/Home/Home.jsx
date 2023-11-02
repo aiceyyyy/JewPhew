86abce0c3ffd90619d7bed18ac8942de2ea3ae07
Jakub Urbański <jakuburbanski2007@gmail.com>
2023-11-02 18:54:04 +0100

    Fixed section anchors

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,16 +5,16 @@ import { HiChevronDoubleDown } from 'react-icons/hi2'
 export const Home = () => {
 	return (
 		<div className='flex flex-col justify-start items-center mt-[100px] lg:mt-[200px] w-[100vw] z-10 mx-auto'>
-			<div className='flex flex-col lg:flex-row mx-auto px-auto'>
+			<div className='flex flex-col xl:flex-row mx-auto px-auto'>
 				<img
 					className='xs:max-w-[250px] w-[250px] h-[250px] mx-auto xs:mt-10 lg:min-w-[400px] lg:w-[400px] lg:h-[400px] mb-0 rounded-2xl'
 					src={img}
 					alt='diamond'
 				/>
-				<div className='h-3/5 mb-5 xs:p-10 lg:px-24'>
+				<div className='h-3/5 mb-5 xs:p-10 xs:text-center xl:text-left lg:px-24'>
 					<h2 className='mb-5 text-4xl uppercase font-thin tracking-wide text-purple-500'>Bestseller</h2>
-					<h1 className='max-w-[300px] mb-5 text-3xl font-bold'>Oppenheimer Blue</h1>
-					<p className='max-w-[400px] text-left'>
+					<h1 className='xs:text-center xl:text-left mb-5 text-3xl font-bold'>Oppenheimer Blue</h1>
+					<p className='max-w-[400px] xs:text-center xl:text-left'>
 						The Oppenheimer Blue is a 14.62-carat (2.924 g) vivid blue diamond that in May 2016 became the most
 						expensive jewel ever sold at auction, until April 2017 when it was surpassed by the Pink Star diamond. The
 						diamond was named for its previous owner Philip Oppenheimer.It is cut into a rectangle (emerald cut).The
--- src/components/Resources/Resources.jsx
+++ src/components/Resources/Resources.jsx
@@ -8,7 +8,7 @@ export const Resources = () => {
 			<div className='max-w-[250px] mx-3 my-3 py-5 px-3 rounded-xl'>
 				<h2 className='text-center text-2xl mb-5 text-purple-500 tracking-wider uppercase'>Diamonds</h2>
 				<img className='h-[200px] rounded-xl' src={path} alt='diamonds' />
-				<p className='mt-3'>
+				<p className='xs:text-center xl:text-left mt-3'>
 					Diamond is a solid form of the element carbon with its atoms arranged in a crystal structure called diamond
 					cubic. Another solid form of carbon known as graphite is the chemically stable form of carbon at room
 					temperature and pressure, but diamond is metastable and converts to it at a negligible rate under those
@@ -18,7 +18,7 @@ export const Resources = () => {
 			<div className='max-w-[250px] mx-20 my-3 py-5 px-3 rounded-xl'>
 				<h2 className='text-center text-2xl mb-5 text-purple-500 tracking-wider uppercase'>Gold</h2>
 				<img className='h-[200px] rounded-xl' src={pic} alt='silver' />
-				<p className='mt-3'>
+				<p className='xs:text-center xl:text-left mt-3'>
 					Gold is a chemical element with the symbol Au (from Latin aurum 'gold') and atomic number 79. It is a bright,
 					slightly orange-yellow, dense, soft, malleable, and ductile metal in pure form. Chemically, gold is a
 					transition metal and a group 11 element.
@@ -28,7 +28,7 @@ export const Resources = () => {
 			<div className='max-w-[250px] mx-3 my-3 py-5 px-3 rounded-xl'>
 				<h2 className='text-center text-2xl mb-5 text-purple-500 tracking-wider uppercase'>Silver</h2>
 				<img className='h-[200px] rounded-xl' src={img} alt='gold' />
-				<p className='mt-3'>
+				<p className='xs:text-center xl:text-left mt-3'>
 					Silver is a chemical element with the symbol Ag (from Latin argentum 'silver', derived from the
 					Proto-Indo-European 'shiny', 'white') and atomic number 47. A soft, white, lustrous transition metal, it
 					exhibits the highest electrical conductivity, thermal conductivity, and reflectivity of any metal.
